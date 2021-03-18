0ec88a828c25853c1f74de84ce7930f072a76ca9
Adhiatmaka Purna Wisuda <adiatmaka@gmail.com>
2021-03-18 22:41:49 +0700

    Tugas 3

--- I0320001_Soal1_Tugas3.py
+++ I0320001_Soal1_Tugas3.py
@@ -1,26 +1,15 @@
-# List teman
 teman = ['Yuku', 'Muza', 'Nadim', 'Rama', 'Evan', 'Upik', 'Dinda', 'Faiz', 'Dila', 'Fina']
 
-
-# Menampilkan list teman nomor 4, 6, dan 7
 print ("Teman ke-4, ke-6, dan ke-7 adalah", teman[4],",", teman[6],",","dan", teman[7])
 
-
-# Mengganti teman
 teman[3] = "Attar"
 teman[5] = "Bagus"
 teman[9] = "Raka"
 
-
-# Menambah teman
 teman.append('Ano')
 teman.append('Ryan')
 
-
-# Menampilkan semua teman dengan perulangan
 print (teman*3)
 
-
-# Menampilkan panjang list teman
 print("Jumlah teman adalah", (len(teman)))
 
