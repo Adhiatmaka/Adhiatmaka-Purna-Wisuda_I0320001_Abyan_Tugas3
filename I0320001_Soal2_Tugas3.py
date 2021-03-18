d820cbbf5884947c6609d6d502f4993ea80db34b
Adhiatmaka Purna Wisuda <adiatmaka@gmail.com>
2021-03-18 22:50:20 +0700

    Tugas 3

--- I0320001_Soal2_Tugas3.py
+++ I0320001_Soal2_Tugas3.py
@@ -1,10 +1,10 @@
 dict = {'Nama':'Adhiatmaka','Hobi':'Basket','Hobii':'Futsal','Hobiii':'Main game', 'Hobiiii': 'Mancing',
-        'Sosmed':'ig:@adhiatmaka','Sosmedd':'igg:@adhiyo','Sosmeddd':'line:@adhiatmaka_',
+        'Sosmed':'ig:@adhiatmaka','Sosmedd':'igg:@adhiyo','Sosmeddd':'tiktok:@adhikaliye',
         'Sosmedddd':'twit:@adhiap','Lagu':'Hope','Laguu':'Mood','Laguuu':'Robbery','Laguuuu': 'Gods Plan' ,
         'Makanan':'Nasi Goreng','Makanann':'Indomie Aceh','Makanannn':'Mie Ayam','Makanannnn': 'Sate Kambing'}
 
 dict['Hobiiii'] = 'Memasak'
-dict['Sosmedd'] = 'tiktok : @adhikaliye'
+dict['Sosmedd'] = 'line : @adhiatm'
 del dict['Makanann']
 del dict['Makanannnn']
 dict['Hobiiiii'] = 'Riding'
